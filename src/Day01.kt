fca80983ef6d4cda3a657901e517138d378bcbd5
Klaas-Hessel Kramer <klaashessel@skrepr.com>
2021-12-02 19:49:08 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun countIncreasingMeasurements(measurements: List<Int>): Int {
+        var previousMeasurement = measurements.first()
+        var total = 0
+        measurements.forEach {
+            if (it > previousMeasurement) {
+                total++
+            }
+
+            previousMeasurement = it
+        }
+
+        return total
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun countGroupedIncreasingMeasurements(measurements: List<Int>): Int {
+        return countIncreasingMeasurements(measurements.windowed(3).map { it.sum() })
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val measurements = readInput("Day01").map { it.toInt() }
+    println(countIncreasingMeasurements(measurements))
+    println(countGroupedIncreasingMeasurements(measurements))
 }
